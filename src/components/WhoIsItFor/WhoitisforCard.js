cf36c75c0dfa43ac0676415637822d3e6988358f
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2020-12-02 18:28:08 +0600

    final-commit

--- src/components/WhoIsItFor/WhoitisforCard.js
+++ src/components/WhoIsItFor/WhoitisforCard.js
@@ -4,7 +4,10 @@ const WhoitisforCard = (props) => {
   const { title } = props.WhoIsItFordata;
   return (
     <div className="col-md-6  bg-white text-dark">
-      <Card className="m-2">
+      <Card
+        className="m-2"
+        style={{ boxShadow: "rgba(16, 60, 105, 0.12) 0px 16px 32px 0px" }}
+      >
         <Card.Body>
           <Card.Title>
             <h5>{title}</h5>
